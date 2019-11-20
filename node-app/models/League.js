85928026f1ae9effa94e40fea33db2e428f24298
Armen Abrahamyan <44841069+Armen-Abrahamyan@users.noreply.github.com>
2019-11-20 21:17:15 +0400

    Update League.js

--- node-app/models/League.js
+++ node-app/models/League.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 const LeagueSchema = new mongoose.Schema({
     // id: Number,
     name: { type: String, unique: true }
-}, { writeConcern: {
-    w: 'majority',
-        j: true,
-        wtimeout: 1000
-}});
+});
 
 module.exports = mongoose.model('League', LeagueSchema);
